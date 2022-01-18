0e3ce280ca09414c20d3ffd89a18587cc39e4086
Szymon Turzański <sz.turzanski@gmail.com>
2022-01-18 19:06:34 +0100

    Poprawa wykresu zenitu

--- proj_2/Projekt2.m
+++ proj_2/Projekt2.m
@@ -17,7 +17,6 @@ lambda = 88.18223614146471;
 %lambda = -51.214916894632374;
 
 h = transpose([0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24]);
-
 % Wartości kątowe dla każdej godziny
 kot_godzinowy = katgodz(2021, 12, 7, h, lambda, rektascensja);
 
@@ -29,15 +28,15 @@ for i = 1:24
 end
 
 % odleglość zenitalna dla każdej godziny
-cos_z = sind(phi).*sind(deklinacja) + cosd(phi).*cosd(deklinacja).*cosd(kot_godzinowy);
-z = acosd(cos_z);
-wysokosc = 90 - z;
+cos_z =  (sind(phi)*sind(deklinacja) + cosd(phi)*cosd(deklinacja).*cosd(kot_godzinowy));
+zenit = acosd(cos_z);
+wysokosc = 90 - zenit;
 
 % Azymut gwiazdy dla każdej godziny
 licznik = -cosd(deklinacja).*sind(kot_godzinowy);
 mianownik = cosd(phi).*sind(deklinacja) - sind(phi).*cosd(deklinacja).*cosd(kot_godzinowy);
 Az = atan2d(licznik, mianownik);
-for i=1:24
+for i = 1:24
    if Az(i) < 0 
        Az(i) = Az(i) + 360; 
    elseif Az(i) > 360
@@ -46,23 +45,23 @@ for i=1:24
 end 
 
 % Obliczenie wsp do układu prostokątnego
-x = 1.*sind(z).*cosd(Az);
-y = 1.*sind(z).*sind(Az);
-z = 1.*cosd(z);
+x = 1.05.*sind(zenit).*cosd(Az);
+y = 1.05.*sind(zenit).*sind(Az);
+z = 1.05.*cosd(zenit);
 
-% Wizualizacja półkuli 
-%[X,Y,Z] = sphere(24);
-%X = X(13:end,:);
-%Y = Y(13:end,:);
-%Z = Z(13:end,:);
-%surf(X,Y,Z,'FaceColor','green','FaceAlpha',0.5)
-%axis equal, 
-%hold on;
-%% Wizualizacja gwiazdy
-%scatter3(x,y,z, 500, 'red', '.')
+%Wizualizacja półkuli 
+[X,Y,Z] = sphere(24);
+X = X(13:end,:);
+Y = Y(13:end,:);
+Z = Z(13:end,:);
+surf(X,Y,Z,'FaceColor','green','FaceAlpha',0.5)
+axis equal, 
+hold on;
+% Wizualizacja gwiazdy
+scatter3(x,y,z, 500, 'red', '.')
 
-plot(h, wysokosc)
-%plot(h, z)
+%plot(h, wysokosc)
+%plot(h, zenit)
 
 
 function [t] = katgodz(y, m, d, h, lambda, alfa)
